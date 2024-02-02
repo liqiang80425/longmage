44593c7af96581a86c7a266a9112273124c83dcf
liqiang <liqiang80425@163.com>
2024-02-02 18:24:56 +0800

    WPFFirst

--- OAManage/OAManage/MainWindow.xaml.cs
+++ OAManage/OAManage/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using OAManage.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +22,39 @@ namespace OAManage
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// 账号model
+        /// </summary>
+        private AccountModel accountModel;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            //设置数据上下文
+            accountModel=new AccountModel();
+            this.DataContext = accountModel;
+        }
+        
+
+        /// <summary>
+        /// 登录(单击登录按钮执行)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Btn_Login(object sender, RoutedEventArgs e)
+        {
+            if (this.accountModel.Account == "longma" && this.accountModel.Pwd == "123")
+            {
+                MessageBox.Show("登录成功");
+            }
+            else
+            {
+                MessageBox.Show("登录失败");
+                //清空账号 密码文本框
+                this.accountModel.Account = "";
+                this.accountModel.Pwd = "";
+            }
         }
     }
 }
---
+++ OAManage/OAManage/Models/AccountModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OAManage.Models
+{
+    /// <summary>
+    /// 用户model  //问题:界面上修改了，后台能接收。后台改了，界面不知道(界面没有接收到通知，INotifyPropertyChanged解决)
+    /// </summary>
+    internal class AccountModel : INotifyPropertyChanged//INotifyPropertyChanged 属性改变通知接口
+    {
+        /// <summary>
+        /// 属性改变事件
+        /// </summary>
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        /// <summary>
+        /// 账号
+        /// </summary>
+        private string _Account;
+
+        /// <summary>
+        /// 账号
+        /// </summary>
+        public string Account
+        {
+            get { return _Account; }
+            set
+            {
+                _Account = value;
+                //通知
+                //PropertyChanged?.Invoke(this,new PropertyChangedEventArgs("Account"));
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this,new PropertyChangedEventArgs("Account"));
+                }
+            }
+        }
+
+        /// <summary>
+        /// 密码
+        /// </summary>
+        private string _Pwd;
+
+        /// <summary>
+        /// 密码
+        /// </summary>
+        public string Pwd
+        {
+            get { return _Pwd; }
+            set { 
+                _Pwd = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("Pwd"));
+                }
+            }
+        }
+
+
+    }
+}
