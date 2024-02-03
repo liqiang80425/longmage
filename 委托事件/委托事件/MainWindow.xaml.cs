0da76cb52f9f6dcfb1cc84eecfa035fc4416d132
liqiang <liqiang80425@163.com>
2024-02-03 12:39:18 +0800

    WPFFirst

---
+++ 委托事件/委托事件/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace 委托事件
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 租房/Model登录  给子窗口/中介执行
+        /// </summary>
+        public void F1()
+        {
+            this.txtTest.Text = "成功";
+            //return 1;
+        }
+
+        public void F2()
+        {
+            this.txtTest.Text = "成功";
+            //return 1;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            SonWin sonWin = new SonWin();
+            sonWin.Dele += F1;//将方法传给子窗口
+            //sonWin.Dele += F2;//将方法传给子窗口
+            sonWin.Show();//打开窗口
+        }
+    }
+}
---
+++ 委托事件/委托事件/SonWin.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace 委托事件
+{
+    /// <summary>
+    /// SonWin.xaml 的交互逻辑
+    /// </summary>
+    public partial class SonWin : Window
+    {
+        ////先定义委托
+        //public delegate void del();//delegate 返回值类型 del(int i,string str);
+
+        //public event del Dele;//委托类型属性   接收主窗口传过来的方法  event: 事件
+
+        //系统委托/预置委托  Aciton、Func
+        //Aciton:没有返回值
+        //Func:必须要有返回值
+        //public Action<int,string> Dele;
+
+        //public Func<int, int, string> Dele1;//第一个是返回值类型。从第二个开始时方法参数（如果方法没有参数，则没有第二个）
+
+        //事件：特殊的委托
+
+        public event Action Dele;
+
+        public SonWin()
+        {
+            InitializeComponent();
+            //this._Dele = Dele;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Dele != null)
+            {
+                Dele();
+            }
+        }
+    }
+}
